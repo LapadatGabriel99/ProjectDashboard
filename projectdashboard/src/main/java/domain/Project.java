a88dddf913cf5d5ea1fa1f7fb90789957ae6651b
Gabi Lapadat <lapadatrobert123@gmail.com>
2020-08-17 14:07:33 +0300

    New commit: Added repositories package and project repository to handle data persistance.

--- projectdashboard/src/main/java/domain/Project.java
+++ projectdashboard/src/main/java/domain/Project.java
@@ -1,4 +1,53 @@
 package domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @Getter
+    private String projectName;
+
+    @Setter
+    @Getter
+    private String projectIdentifier;
+
+    @Setter
+    @Getter
+    private String description;
+
+    @Setter
+    @Getter
+    private Date startDate;
+
+    @Setter
+    @Getter
+    private Date endDate;
+
+    @Setter
+    @Getter
+    private Date createdAt;
+
+    @Setter
+    @Getter
+    private Date updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = new Date();
+    }
 }
---
+++ projectdashboard/src/main/java/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package repositories;
+
+import domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAll();
+}
