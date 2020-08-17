b2d1f8fc65750a9fa30fe6addeae5e14d4318c1b
Gabi Lapadat <lapadatrobert123@gmail.com>
2020-08-17 13:42:29 +0300

    Second commit.

---
+++ projectdashboard/src/test/java/com/example/dashBoard/DashBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dashBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
